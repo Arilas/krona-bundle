4bf950441ef47ec1e21728ed07dd7fbbe7fbc4a4
Alex Grand <Kiral.Group@gmail.com>
2014-06-11 05:57:20 +0300

    added Runtime Exception for Krona Bundle

---
+++ src/Exception/KronaRuntimeException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: krona
+ * Date: 6/11/14
+ * Time: 5:56 AM
+ */
+
+namespace Arilas\KronaBundle\Exception;
+
+use RuntimeException;
+
+class KronaRuntimeException extends RuntimeException
+{
+
+} 
